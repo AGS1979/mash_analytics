a5453cdd0eb5717e6cf96f83b5850ca4f8223668
Avinash Singh <avi104@yahoo.co.in>
2025-07-11 19:47:03 +0530

    SS agent

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -774,7 +774,7 @@ else if (agent.id === "Special_Situations_Analyzer") {
                     <option value="">-- Select --</option>
                     <option value="Spin-Off or Split-Up">Spin-Off or Split-Up</option>
                     <option value="Mergers & Acquisitions">Mergers & Acquisitions</option>
-                    <option value="Bankruptcy / Restructuring">Bankruptcy / Restructuring</option>
+                    <option value="Bankruptcy / Distressed / Restructuring">Bankruptcy / Restructuring</option>
                     <option value="Activist Campaign">Activist Campaign</option>
                     <option value="Regulatory or Legal Catalyst">Regulatory or Legal Catalyst</option>
                     <option value="Asset Sales or Carve-Outs">Asset Sales or Carve-Outs</option>
